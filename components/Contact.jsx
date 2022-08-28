f6395229a7e56c19ba82c5419d28702144cdf306
sumarius56 <110302280+sumarius56@users.noreply.github.com>
2022-08-28 21:44:48 +0300

    changed linked in url

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -39,7 +39,7 @@ const Contact = () => {
                 <p className='uppercase pt-8'>Connect With Me</p>
                 <div className='flex items-center justify-between py-4'>
                   <a
-                    href='https://www.linkedin.com/in/suciu-marius-72a840245/'
+                    href='www.linkedin.com/in/suciumariuscornel/'
                     target='_blank'
                     rel='noreferrer'
                   >
